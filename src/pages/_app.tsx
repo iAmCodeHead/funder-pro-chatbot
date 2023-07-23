693f4ad8a89e2dc3fbfeeefc4f2c63a13cbb3b8d
akande samson <akandesamson12@gmail.com>
2023-07-23 13:29:29 +0200

    remove unused font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,6 @@
 import '@/styles/base.css';
 
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
-
-// eslint-disable-next-line unused-imports/no-unused-vars
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-});
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
